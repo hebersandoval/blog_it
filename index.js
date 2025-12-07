3110c926dfed761fcacf55e8f3a0c092699d0009
Heber Sandoval <he3ers@gmail.com>
2025-12-06 22:08:24 -0600

    Deleted Node server code.

--- index.js
+++ index.js
@@ -22,34 +22,3 @@ app.get('/contact', (request, response) => {
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
-
-/*
-const http = require('http');
-const fs = require('fs');
-
-const aboutPage = fs.readFileSync('about.html');
-const contactPage = fs.readFileSync('contact.html');
-const homePage = fs.readFileSync('index.html');
-const pageNotFound = fs.readFileSync('404.html');
-
-// Create server
-const server = http.createServer((request, response) => {
-    // request - from client (browser) to server
-    // response - from server to client
-    console.log(request.url);
-
-    if (request.url === '/about') {
-        return response.end(aboutPage);
-    } else if (request.url === '/contact') {
-        return response.end(contactPage);
-    } else if (request.url == '/') {
-        return response.end(homePage);
-    } else {
-        response.writeHead(404);
-
-        response.end(pageNotFound);
-    }
-});
-
-server.listen(3000);
-*/
