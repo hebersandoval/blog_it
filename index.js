985580acbd8ac5b1f1db32cdce83891e2d714abd
Heber Sandoval <he3ers@gmail.com>
2025-12-06 20:53:18 -0600

    Fixed typo.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Use assests
+// Use assets
 app.use(express.static('public'));
 
 app.get('/', (request, response) => {
