477354416799720a6bd4efbde0b626cdfa6b56b9
Heber Sandoval <he3ers@gmail.com>
2025-12-07 08:35:30 -0600

    Install express-edge templating engine.

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const path = require('path');
 const express = require('express');
 
+const templeEngine = require('express-edge');
+
 const app = express();
 const port = 3000;
 
 // Use assets
 app.use(express.static('public'));
+
+// Sets view engine
+app.use(templeEngine);
+app.set('view engine', 'hbs');
+app.set('views', `${__dirname}/views`);
 
 app.get('/', (request, response) => {
     response.sendFile(path.resolve(__dirname, 'pages/index.html'));
