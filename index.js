179f1c11220d1b2f7cc9c38213e0bcabf2b29955
Heber Sandoval <he3ers@gmail.com>
2025-12-06 22:47:07 -0600

    Moved a few files.

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const port = 3000;
 app.use(express.static('public'));
 
 app.get('/', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'index.html'));
+    response.sendFile(path.resolve(__dirname, 'pages/index.html'));
 });
 
 app.get('/about', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'about.html'));
+    response.sendFile(path.resolve(__dirname, 'pages/about.html'));
 });
 
 app.get('/contact', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'contact.html'));
+    response.sendFile(path.resolve(__dirname, 'pages/contact.html'));
 });
 
 app.listen(port, () => {
