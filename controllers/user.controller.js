4bd59c85bd38751ea96f56666ce1adeb73978b22
Heber Sandoval <he3ers@gmail.com>
2025-12-13 06:16:35 -0600

    Cleaned up some files.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,28 +1,5 @@
 const User = require('../libs/models/user.model');
 
-/*
-const createUser = async (request, response) => {
-    await User.create({
-        email: 'bob@mail.com',
-        password: 'secret',
-    });
-
-    response.render('user', { message: 'User created', user: null });
-};
-
-const getUser = async (request, response) => {
-    const user = await User.findOne({ email: 'bob@mail.com' }).lean();
-
-    response.render('user', { message: 'User retrieved', user: user });
-};
-
-const deleteUser = async (request, response) => {
-    await User.findOneAndDelete({ email: 'bob@mail.com' });
-
-    response.render('user', { message: 'User deleted', user: null });
-};
-*/
-
 const signup = async (request, response) => {
     const { email, password } = request.body;
     const query = { email };
