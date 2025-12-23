d5a236869ec58f8b407ab11d9504c95265549157
Heber Sandoval <he3ers@gmail.com>
2025-12-23 15:20:35 -0600

    Nested customer under dashboard and create routes.

---
+++ routes/customer.route.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const { showCustomers } = require('../controllers/customer.controller');
+
+router.get('/', showCustomers);
+
+module.exports = router;
--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -2,11 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 
+const customersRouter = require('./customer.route');
+
 router.get('/', (request, response) => {
     response.render('pages/dashboard', {
         title: 'Dashboard',
         info: request.flash('info')[0],
     });
 });
+
+// Customer nested under dashboard -> mysite.com/dashboard/customers
+router.use('/customers', customersRouter);
 
 module.exports = router;
